6aa3eab7b27a9a5a910013b9ccf5c5c94433f23a
franciele <francielefernandes126@gmail.com>
2023-03-01 13:58:39 -0300

    🌈 style: Correção no favicon

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Sistema') }}</title>
+    <title>Sistemas de Produtos</title>
+    <link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon">
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
